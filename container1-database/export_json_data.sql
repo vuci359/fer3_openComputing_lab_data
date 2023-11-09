36baef1f30b2c49906788a9718c585cd7915976d
Karlo Vučilovski <Karlo.Vucilovski@FER.hr>
2023-11-09 22:55:10 +0100

    ugly mess successfully made

--- container1-database/export_json_data.sql
+++ container1-database/export_json_data.sql
@@ -1,17 +1,17 @@
 COPY(
 	select array_to_json(array_agg(row_to_json(music_data))) from(
 		select
-			 sng.name-- as "song name"
-			,bnd.band_name --as "band name"
-			,bnd.members-- as "band members"
-			,bnd.genre-- as "genre"
-			,alb.name-- as "album name"
-			,alb.label-- as "album label"
-			,alb.date_released-- as "album release date"
-			,sng.length-- as "song length"
-			,sng.no_on_album-- as "position on album"
-			,sng.lyrics_writers-- as "lyrics writers"
-			,sng.music_writers-- as "music writers"
+			 sng.name as "song name"
+			,bnd.band_name as "band name"
+			,bnd.members as "band members"
+			,bnd.genre as "genre"
+			,alb.name as "album name"
+			,alb.label as "album label"
+			,alb.date_released as "album release date"
+			,sng.length as "song length"
+			,sng.no_on_album as "position on album"
+			,sng.lyrics_writers as "lyrics writers"
+			,sng.music_writers as "music writers"
 			,sng.lyrics as "lyrics"
 
 		from "Band" as bnd
--- container3-backend/App/Controllers/HeavyMetalDataController.cs
+++ container3-backend/App/Controllers/HeavyMetalDataController.cs
@@ -1,10 +1,8 @@
-using System.Collections.Generic;
-using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using System.Runtime.Versioning;
 using openComputingLab.Models;
 using openComputingLab.Data;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Npgsql;
 
 namespace openComputingLab.Controllers;
 
@@ -15,5 +13,33 @@ public class HeavyMetalDataController: ControllerBase{
 
     public HeavyMetalDataController(AppDbContext dbContext){
         _dbContext = dbContext;
+    }
+    [HttpGet]
+    [ActionName("GetUnfilteredData")]
+    public IActionResult GetData(string stupac = "", string parameter = "*"){
+        string sql_query_select = @"select sng.name as ""song_name""
+			, bnd.band_name as ""band_name""
+			,unnest(bnd.members) as ""band_members""
+			,bnd.genre as ""genre""
+			,alb.name as ""album name""
+			,alb.label as ""album label""
+			,alb.date_released as ""album release date""
+			,sng.length as ""song length""
+			,sng.no_on_album as ""position on album""
+			,unnest(sng.lyrics_writers) as ""lyrics writers""
+			,unnest(sng.music_writers) as ""music writers""
+			,sng.lyrics as ""lyrics""
+		from ""Band"" as bnd
+		join ""Album"" as alb on bnd.ident = alb.band_ident
+		join ""Song"" as sng on alb.ident = sng.album_ident";
+		string sql_query_sort = " order by sng.name asc;";
+		string sql_query_filter = "";
+		if (stupac != "") sql_query_filter = " where"+stupac+"like"+parameter+" ";
+
+		string sql_query = sql_query_select+sql_query_filter+sql_query_sort;
+		Console.WriteLine(sql_query);
+
+
+        return Ok();
     }
 }
