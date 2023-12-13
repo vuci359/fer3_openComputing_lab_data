535603b3909eb7f40acb5b2485cd592a01a66aaa
Karlo Vučilovski <Karlo.Vucilovski@FER.hr>
2023-12-13 18:56:23 +0100

    done preaparation work for new Controllers

---
+++ container3-backend/App/Controllers/BandController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using openComputingLab.Data;
+using Newtonsoft.Json;
+using System.Text.Json.Nodes;
+using Microsoft.AspNetCore.Cors;
+using openComputingLab.Models;
+using Microsoft.EntityFrameworkCore;
+
+//using Newtonsoft.Json;
+
+namespace openComputingLab.Controllers;
+
+[ApiController]
+[Route("api/[controller]/[action]")]
+[Produces("application/json")]
+[EnableCors]
+
+public class BandController : ControllerBase
+{
+    private readonly AppDbContext _dbContext;
+
+    public BandController(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    [HttpGet]
+    [ActionName("GetData")]
+    public IActionResult GetData(int ? stupac = null, string ? parameter = null){
+        try{
+            return Ok(_dbContext.Bands.ToArray());
+
+        }catch(Exception e){
+            Console.WriteLine(e.StackTrace);
+            return NotFound();
+        }
+
+        return Ok();
+    }
+}
+
+//		where {stupac} like {parameter}
