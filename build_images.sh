b63ad9d9b30b015ab542d7a9829668f320345e82
Karlo Vučilovski <Karlo.Vucilovski@FER.hr>
2023-12-18 14:39:08 +0100

    prepared to merge with master

--- build_images.sh
+++ build_images.sh
@@ -4,13 +4,13 @@
 #docker rm -f $(docker ps -a -q)
 
 #build and upload database
-docker build -t vuci359/fer3_opencomputing_lab_exam:database_v1 ./container1-database/
-docker push vuci359/fer3_opencomputing_lab_exam:database_v1
+docker build -t vuci359/fer3_opencomputing_lab_exam:database_v3 ./container1-database/
+docker push vuci359/fer3_opencomputing_lab_exam:database_v3
 
 #build and upload backend
-docker build -t vuci359/fer3_opencomputing_lab_exam:backend_v1 ./container3-backend/App/
-docker push vuci359/fer3_opencomputing_lab_exam:backend_v1
+docker build -t vuci359/fer3_opencomputing_lab_exam:backend_v3 ./container3-backend/App/
+docker push vuci359/fer3_opencomputing_lab_exam:backend_v3
 
 #build and upload frontend
-docker build -t vuci359/fer3_opencomputing_lab_exam:frontend_v1 ./container2-frontend/
-docker push vuci359/fer3_opencomputing_lab_exam:frontend_v1
+docker build -t vuci359/fer3_opencomputing_lab_exam:frontend_v3 ./container2-frontend/
+docker push vuci359/fer3_opencomputing_lab_exam:frontend_v3
